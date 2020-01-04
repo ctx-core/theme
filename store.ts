3cfb6c156d20a7f0b295becf3eb7cc57b4eb1374
Brian Takita <brian.takita@gmail.com>
2020-01-04 17:22:06 -0500

    feat: store: b__ factories

--- store.ts
+++ store.ts
@@ -1,12 +1,27 @@
-import { writable, derived } from 'svelte/store'
+import { writable, derived, Writable } from 'svelte/store'
 // @ts-ignore
 import { subscribe__debug } from '@ctx-core/store'
+import { _b, assign } from '@ctx-core/object'
 import { _class } from '@ctx-core/html'
 import { not } from '@ctx-core/function'
-export const __theme__invert = writable(null)
-export function invert__theme() {
-  __theme__invert.update(not)
+export interface Writable__theme__invert extends Writable<boolean> {
+	invert__theme:()=>void
 }
-export const __class__theme__invert =
-  derived(__theme__invert,
-    theme__invert => _class({ theme__invert }))
+export const b__theme__invert = _b<Writable__theme__invert>('__theme__invert', ()=>{
+	const __theme__invert = writable(false)
+	return assign(__theme__invert, {
+		invert__theme,
+	})
+	function invert__theme() {
+		__theme__invert.update(not)
+	}
+})
+export const __theme__invert = b__theme__invert()
+export const {
+	invert__theme,
+} = __theme__invert
+export const b__class__theme__invert = _b('__class__theme__invert', ctx=>
+	derived(
+		b__theme__invert(ctx),
+		theme__invert=>
+			_class({ theme__invert })))
