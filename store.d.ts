b84eff0cddd71d85251853a0239e16f17564dabf
Brian Takita <brian.takita@gmail.com>
2020-10-02 16:44:00 -0700

    type updates

--- store.d.ts
+++ store.d.ts
@@ -6,5 +6,5 @@ export interface type__theme__invert extends Writable<$type__theme__invert> {
 export declare const b__theme__invert: (ctx?: object | undefined, opts?: import("@ctx-core/object")._be_opts_type | undefined) => type__theme__invert;
 export declare const __theme__invert: type__theme__invert;
 export declare const invert__theme: () => void;
-export declare const b__class__theme__invert: (ctx?: object | undefined, opts?: import("@ctx-core/object")._be_opts_type | undefined) => any;
-export declare const __class__theme__invert: any;
+export declare const b__class__theme__invert: (ctx?: object | undefined, opts?: import("@ctx-core/object")._be_opts_type | undefined) => import("@ctx-core/store").Readable<string>;
+export declare const __class__theme__invert: import("@ctx-core/store").Readable<string>;
