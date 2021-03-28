24bcbd854d59868474f4efcf7abbc251521c24dc
Brian Takita <brian.takita@gmail.com>
2021-03-28 18:14:12 -0400

    update dependencies

--- src/invert_theme_b.d.ts
+++ src/invert_theme_b.d.ts
@@ -1,5 +1,5 @@
 import { Writable } from '@ctx-core/store';
-export declare const invert_theme_b: import("@ctx-core/object").be_type<invert_theme_type, object>;
+export declare const invert_theme_b: import("@ctx-core/object").Be<invert_theme_type, object>;
 export { invert_theme_b as b__theme__invert };
 export declare type $invert_theme_type = boolean;
 export interface invert_theme_type extends Writable<$invert_theme_type> {
--- src/invert_theme_class_b.d.ts
+++ src/invert_theme_class_b.d.ts
@@ -1,2 +1,2 @@
-export declare const invert_theme_class_b: import("@ctx-core/object").be_type<import("@ctx-core/store").Readable<string>, object>;
+export declare const invert_theme_class_b: import("@ctx-core/object").Be<import("@ctx-core/store").Readable<string>, object>;
 export { invert_theme_class_b as b__class__theme__invert };
