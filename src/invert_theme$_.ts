b609ace2d23a414be4c19df6c1ddde0c91955077
Brian Takita <brian.takita@gmail.com>
2022-05-30 01:40:27 -0500

    update dependencies

--- src/invert_theme$_.ts
+++ src/invert_theme$_.ts
@@ -1,7 +1,7 @@
 import { assign, B, be_ } from '@ctx-core/object'
-import { atom$, WritableAtom$ } from '@ctx-core/nanostores'
+import { atom_, WritableAtom_ } from '@ctx-core/nanostores'
 export const invert_theme$_:B<invert_theme$_T> = be_('invert_theme$', ()=>{
-	const invert_theme$ = atom$(false) as invert_theme$_T
+	const invert_theme$ = atom_(false) as invert_theme$_T
 	return assign(invert_theme$, {
 		toggle_invert_theme,
 		invert__theme: toggle_invert_theme,
@@ -10,6 +10,6 @@ export const invert_theme$_:B<invert_theme$_T> = be_('invert_theme$', ()=>{
 		invert_theme$.$ = !invert_theme$.$
 	}
 })
-export interface invert_theme$_T extends WritableAtom$<boolean> {
+export interface invert_theme$_T extends WritableAtom_<boolean> {
 	invert_theme:()=>void
 }
--- src/invert_theme_class$_.ts
+++ src/invert_theme_class$_.ts
@@ -1,10 +1,10 @@
 import { class_ } from '@ctx-core/html'
-import { computed$, ReadableAtom$ } from '@ctx-core/nanostores'
+import { computed_, ReadableAtom_ } from '@ctx-core/nanostores'
 import { B, be_ } from '@ctx-core/object'
 import { invert_theme$_ } from './invert_theme$_.js'
 export const invert_theme_class$_:B<invert_theme_class$_T> = be_('invert_theme_class$', ctx=>
-	computed$(
+	computed_(
 		invert_theme$_(ctx),
 		invert_theme=>
 			class_({ invert_theme })))
-export type invert_theme_class$_T = ReadableAtom$<string>
+export type invert_theme_class$_T = ReadableAtom_<string>
